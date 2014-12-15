fcff71d5541d60d55ba2a031f2791d4f047013b2
s6070094 <s6070094@kmitl.ac.th>
2014-12-15 21:15:20 +0700

    ทำหน้าจอหลักโปรแกรมนี้

--- project.py
+++ project.py
@@ -3,18 +3,12 @@ from Tkinter import *
 root = Tk() #หน้าจอหลักโปรแกรม
 root.geometry('780x580+280+70')
 root.title("How Smoking Affects Your Life")
-root.configure(bg='light green')
-label1 = Label(root, text="ยินดีต้อนรับเข้าสู่แอพ",fg='blue',bg = 'light blue')
-label1.grid(row=0,column=0)
-def start(): #หน้าที่2 --> หน้ากรอกข้อมูลพร้อมปุ่มคำนวณ
-    main = Tk()
-    main.title("How Smoking Affects Your Life")
-    main.geometry('780x580+280+70')
-    main.configure(bg='light green')
-    label2 = Label(main, text="คุณสูบบุหรี่เฉลี่ย",fg='blue',bg = 'light blue')
-    label2.grid(row=0,column=0)
-button1 = Button(root, text="เข้าสู่หน้าหลัก",fg='red',command = start)
-button1.grid(row=1,column=2)
-button2 = Button(root, text="ออกจากโปรแกรม",fg='red',command = exit)
-button2.grid(row=1,column=3)
+root.configure(bg='light blue')
+label1 = Label(root, text="How Smoking Affects Your Life!",fg='blue',bg = 'light blue', font = "Times 30 bold").place(x=165, y=200)
+labelna = Label(root, text="โปรแกรมสำหรับผู้ที่ต้องการเลิกบุหรี่",fg='blue',bg = 'light blue', font = "Times 30 bold").place(x=160, y=252)
+button1 = Button(root, text="เข้าสู่โปรแกรม",command = start, font = "Times 15 bold").place(x=200, y=330)
+def exit1():
+    root.destroy()
+button2 = Button(root, text="ออกจากโปรแกรม",command = exit1, font = "Times 15 bold").place(x=430, y=330)
+    
 root.mainloop()
