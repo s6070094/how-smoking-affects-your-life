39ed11e1deb20be8d95aeb407ad6f76eec166234
= <s7070123@kmitl.ac.th>
2014-12-16 18:34:04 +0700

    finish program

--- project.py
+++ project.py
@@ -47,14 +47,33 @@ def start(): #หน้าที่2 --> หน้ากรอกข้อมู
         label4 = Label(pop, text="ชีวิตลดลงเฉลี่ย " + str(process_2() + " ต่อปี"), font = "Times 45 bold",fg = 'red',bg ='light pink').place(x=70, y=200)
         label_money = Label(pop, text="   สูญเสียเงินเฉลี่ย " + str(process_3() + " บาท ต่อปี"), font = "Times 30 ",fg = 'purple',bg = 'light pink').place (x=170,y=260)
         label5 = Label(pop, text="คุณต้องการคำแนะนำการเลิกบุหรี่หรือไม่?", font = "Times 20 bold",fg = 'blue',bg = 'light pink').place(x=220, y=310)
+        def yes():#หน้าสุดท้าย --> แนะนำการเลิกบุหรี่
+            main.destroy()
+            pop.destroy()
+            last = Tk()
+            last.configure(bg = "light yellow")
+            last.title("How Smoking Affects Your Life")
+            last.geometry('780x580+280+70')
+            label6 = Label(last, text="คำแนะนำ", font = "Times 30 bold",fg = 'dark green',bg = 'light yellow').place(x=310 ,y=20)
+            label7 = Label(last, text="1.ขอคำปรึกษาจากคนที่สามารถเลิกสูบบุหรี่ได้สำเร็จมาแล้ว หรือขอคำแนะนำในการเลิกบุหรี่ได้ที่ โทร.1600", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=70)
+            label8 = Label(last, text="2.ควรบอกให้คนใกล้ชิดทราบถึงความตั้งใจที่จะเลิกบุหรี่ เพราะกำลังใจจากคนรอบข้างจะช่วยให้คุณมีความพยายามที่จะเลิกสูบบุหรี่ได้", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=110)
+            label9 = Label(last, text="3.ควรวางแผนการปฏิบัติตัว โดยกำหนดวันที่จะลงมือเลิกสูบบุหรี่ เช่น วันเกิดตัวเองหรือของลูก", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=150)
+            label10 = Label(last, text="4.ควรเตรียมตัวให้พร้อม ด้วยการทิ้งอุปกรณ์ที่เกี่ยวข้องกับบุหรี่ให้หมด เตรียมผลไม้รสเปรี้ยวหรือของขบเคี้ยว เพื่อช่วยลดความอยาก", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=190)
+            label11 = Label(last, text="5.เมื่อถึงวันลงมือ ควรตื่นนอนด้วยความสดชื่น บอกตัวเองว่าคุณกำลังทำสิ่งที่ดีที่สุดให้กับตนเองและคนใกล้ชิด", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=230)
+            label12 = Label(last, text="6.ในระหว่างนี้ขอให้คุณหลีกเลี่ยงกิจกรรมที่จะทำให้คุณอยากสูบบุหรี่ เช่น ถ้าเคยดื่มกาแฟ หรือเครื่องดื่มที่มีแอลกอฮอล์", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=270)
+            label13 = Label(last, text="7.เมื่อรู้สึกเครียด ให้หยุดพักสมองสักครู่ คลายความเครียด ระลึกถึงเสมอว่า มีคนไม่สูบบุหรี่อีกมากที่คลายเครียดได้โดยไม่ต้องสูบบุหรี่", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=310)
+            label14 = Label(last, text="8.ควรจัดเวลาออกกำลังกายบ้างอย่างน้อยวันละ 15-20 นาที เพราะนอกจากจะเป็นการควบคุมน้ำหนัก ยังช่วยให้สมองปลอดโปร่ง", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=350)
+            label15 = Label(last, text="9.อย่าคิดว่ากลับไปลองสูบบ้างเป็นครั้งคราวคงไม่เป็นไร เพราะการลองสูบบุหรี่เพียงมวนเดียวอาจหมายถึงการหวนคืนไปสูความเคยชินเก่าๆ", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=390)
+            label16 = Label(last, text="10.หากต้องหันกลับไปสูบอีก ไม่ได้หมายถึงคุณเป็นคนล้มเหลว อย่างน้อยคุณก็ได้เรียนรู้ที่จะปรับปรุงตัวเอง ขอเพียงพยายามต่อไป", font = "Times 13 bold",fg = 'dark green',bg = 'light yellow').place(x=15 ,y=430)
+            def ex():
+                last.destroy()
+            buttonex = Button(last, text = "ออกจากโปรแกรม",command = ex, font = "Times 15 bold").place(x=320,y=490)
         button3 = Button(pop,text = "ต้องการ", command = yes, font = "Times 15 bold").place(x=300, y=360)
         def exitall():
             pop.destroy()
             main.destroy()
         button4 = Button(pop,text = "ไม่ต้องการ", command = exitall, font = "Times 15 bold").place(x=410, y=360) 
-
     button_main = Button(main, text="คำนวณ",command = process, font = "Times 25 bold").place(x=340, y=350)
-
 button1 = Button(root, text="เข้าสู่โปรแกรม",command = start, font = "Times 15 bold").place(x=200, y=330)
 def exit1():
     root.destroy()
